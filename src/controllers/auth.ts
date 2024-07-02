f806d7d2cc5146346322e8830840287c5bbd5675
aliseyedabady <ialiseyed@gmail.com>
2024-07-02 20:38:58 +0330

    add verifyOTPAndLogin to AuthController

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -8,9 +8,12 @@ class AuthController {
   async sendOTP(req: Request, res: Response) {
     const { mobile } = req.body;
     try {
-      const user = await User.findOne({ mobile });
+      let user = await User.findOne({ mobile });
       if (!user) {
-        return ResponseHandler.notFound(res, "Mobile Not Found!");
+        user = new User({
+          mobile,
+        });
+        await user.save();
       }
 
       // Send the OTP to the user via email or SMS here
@@ -24,6 +27,24 @@ class AuthController {
     } catch (error) {
       return ResponseHandler.error(res, error);
     }
+  }
+  async verifyOTPAndLogin(req: Request, res: Response) {
+    const { mobile, otp } = req.body;
+    try {
+      const user = await User.findOne({ mobile });
+      if (!user) {
+        return ResponseHandler.notFound(res, "Mobile Not Found!");
+      }
+      if (
+        user.otp &&
+        user.otp.code === otp &&
+        moment(user.otp.expiresAt).isAfter(moment())
+      ) {
+        user.otp = undefined;
+        await user.save();
+        return ResponseHandler.success(res, {});
+      }
+    } catch (error) {}
   }
 }
 
