4b06751601d4db8c84f01a4de2f7a1015c243b39
aliseyedabady <ialiseyed@gmail.com>
2024-07-02 19:54:40 +0330

    Add express-validator and Response Handler

---
+++ src/models/ResponseHanlder.ts
@@ -0,0 +1,68 @@
+import { Response } from "express";
+import { Result, ValidationError } from "express-validator";
+
+class ResponseHandler {
+  success(
+    res: Response,
+    data = {},
+    message = "Success",
+    statusCode = 200
+  ): Response {
+    return res.status(statusCode).json({
+      status: "success",
+      message,
+      data,
+    });
+  }
+
+  error(
+    res: Response,
+    error: any,
+    message = "Server Error",
+    statusCode = 500
+  ): Response {
+    return res.status(statusCode).json({
+      status: "error",
+      message,
+      error,
+    });
+  }
+
+  notFound(
+    res: Response,
+    message = "Resource not found",
+    statusCode = 404
+  ): Response {
+    return res.status(statusCode).json({
+      status: "error",
+      message,
+    });
+  }
+
+  badRequest(
+    res: Response,
+    message = "Bad Request",
+    statusCode = 400
+  ): Response {
+    return res.status(statusCode).json({
+      status: "error",
+      message,
+    });
+  }
+
+  validationError(
+    res: Response,
+    message: Result<ValidationError>,
+    statusCode = 422
+  ) {
+    return res.status(statusCode).json({
+      status: "error",
+      message: message.array(),
+    });
+  }
+  unauthorized(res: Response, statusCode = 401) {
+    return res.status(statusCode);
+  }
+}
+
+export default new ResponseHandler();
