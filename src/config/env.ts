a5117699dc7020e36d69cd835c870321c129e305
aliseyedabady <ialiseyed@gmail.com>
2024-07-02 19:46:03 +0330

    Create AuthController && otp utils

--- src/config/env.ts
+++ src/config/env.ts
@@ -6,12 +6,14 @@ interface EnvConfig {
   PORT: number;
   MONGO_URI: string;
   DB_Name: string;
+  OTP_LENGTH: number;
 }
 
 const config: EnvConfig = {
   PORT: Number(process.env.PORT) || 5000,
   MONGO_URI: process.env.MONGO_URI || "",
   DB_Name: process.env.DB_NAME || "",
+  OTP_LENGTH: Number(process.env.OTP_LENGTH) || 6,
 };
 
 if (!config.MONGO_URI) {
@@ -19,7 +21,7 @@ if (!config.MONGO_URI) {
 }
 
 if (!config.DB_Name) {
-    throw new Error("Missing DB_NAME in environment variables");
-  }
-  
+  throw new Error("Missing DB_NAME in environment variables");
+}
+
 export default config;
---
+++ src/controllers/auth.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from "express";
+
+class AuthController {
+  async login(req: Request, res: Response) {}
+}
+
+export default new AuthController();
