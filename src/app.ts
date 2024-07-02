8f788f4a90781c4ddbe248fb4c12697fb84eb73b
aliseyedabady <ialiseyed@gmail.com>
2024-07-02 18:36:38 +0330

    Add dotenv and move mogodb connect to config folder

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,24 @@
 import express from "express";
 import bodyParser from "body-parser";
-import mongoose from "mongoose";
+import connectDB from "./config/database";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 
 const app = express();
-const port = 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose
-  .connect("mongodb://localhost:27017", {
-    dbName: "e-commerce",
-  })
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log(err));
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
