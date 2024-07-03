d090406cd3e953a0d62a74e5cbb61811e891b954
aliseyedabady <ialiseyed@gmail.com>
2024-07-03 13:47:11 +0330

    Add signUp and validation response to auth methods

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -5,6 +5,7 @@ import moment from "moment";
 import config from "../config/env";
 import jwt from "jsonwebtoken";
 import { generateAccessToken } from "../lib";
+import { validationResult } from "express-validator";
 
 class AuthController {
   async sendOTP(req: Request, res: Response) {
@@ -79,6 +80,10 @@ class AuthController {
   async adminLogin(req: Request, res: Response) {
     const { mobile, password } = req.body;
     try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return ResponseHandler.validationError(res, errors);
+      }
       const user = await User.findOne({ mobile });
       if (!user) {
         return ResponseHandler.badRequest(res, "mobile or password is wrong!");
@@ -99,6 +104,19 @@ class AuthController {
         }
       }
       return ResponseHandler.badRequest(res, "mobile or password is wrong!");
+    } catch (error) {
+      return ResponseHandler.error(res, error);
+    }
+  }
+
+  async signup(req: Request, res: Response) {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return ResponseHandler.validationError(res, errors);
+      }
+      const user = new User(req.body);
+      await user.save();
     } catch (error) {
       return ResponseHandler.error(res, error);
     }
