389b38b064da49e37e923f22679cadfceedf1e81
aliseyedabady <ialiseyed@gmail.com>
2024-07-04 12:17:19 +0330

    Add ProductController structure

---
+++ src/controllers/product/index.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+
+class ProductController {
+  async create() {}
+  async findAll() {}
+  async findOne() {}
+  async update() {}
+  async delete() {}
+}
+
+export default new ProductController();
