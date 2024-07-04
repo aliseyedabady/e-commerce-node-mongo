2c7f1f8a742f375c6cac88bb547ef475ed5049b7
aliseyedabady <ialiseyed@gmail.com>
2024-07-04 12:46:46 +0330

    Serve "/uploads" address in http

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-connectDB();
+app.use("/uploads", express.static("uploads"));
 
 app.use(api);
 
