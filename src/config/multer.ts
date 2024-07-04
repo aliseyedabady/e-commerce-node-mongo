185a3ad8b969d1e39684b4d95f7639744b242779
aliseyedabady <ialiseyed@gmail.com>
2024-07-04 12:46:58 +0330

    Config multer

---
+++ src/config/multer.ts
@@ -0,0 +1,37 @@
+import multer from "multer";
+import path from "path";
+
+const storage = multer.diskStorage({
+  destination: "./uploads/",
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
+
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1000000 },
+  fileFilter: (req, file, cb) => {
+    checkFileType(file, cb);
+  },
+}).array("images", 10);
+
+function checkFileType(
+  file: Express.Multer.File,
+  cb: multer.FileFilterCallback
+) {
+  const filetypes = /jpeg|jpg|png|webp|gif/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
+
+  if (extname && mimetype) {
+    return cb(null, true);
+  } else {
+    cb(Error("Error: Images Only!"));
+  }
+}
+
+export default upload;
