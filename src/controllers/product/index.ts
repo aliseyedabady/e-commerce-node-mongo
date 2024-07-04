a3c3218195b983affc37f6d2909d368e9db13f8f
aliseyedabady <ialiseyed@gmail.com>
2024-07-04 12:47:25 +0330

    Add upload method to create method ProductController

--- src/controllers/product/index.ts
+++ src/controllers/product/index.ts
@@ -1,7 +1,15 @@
 import { Request, Response } from "express";
+import upload from "../../config/multer";
+import { ResponseHandler } from "../../lib";
 
 class ProductController {
-  async create() {}
+  async create(req: Request, res: Response) {
+    upload(req, res, async (err) => {
+      if (err) {
+        return ResponseHandler.error(res, err);
+      }
+    });
+  }
   async findAll() {}
   async findOne() {}
   async update() {}
