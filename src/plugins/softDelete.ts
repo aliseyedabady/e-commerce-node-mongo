208952e0f0d1ba095bf0c4670b0482960dbc618b
aliseyedabady <ialiseyed@gmail.com>
2024-07-07 18:53:45 +0330

    Add a custom plugin for soft delete

---
+++ src/plugins/softDelete.ts
@@ -0,0 +1,12 @@
+import { Schema, Query } from "mongoose";
+
+export const softDeletePlugin = (schema: Schema): void => {
+  schema.add({ isDeleted: { type: Boolean, default: false } });
+  const excludeDeletedItems = function (this: Query<any, any>) {
+    this.where({ isDeleted: { $ne: true } });
+  };
+  schema.pre<Query<any, any>>('find', excludeDeletedItems);
+  schema.pre<Query<any, any>>('findOne', excludeDeletedItems);
+  schema.pre<Query<any, any>>('findOneAndUpdate', excludeDeletedItems);
+  schema.pre<Query<any, any>>('countDocuments', excludeDeletedItems);
+};
