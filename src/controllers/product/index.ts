24cb15462ab6589dffca6a4fe6346556ed5f3ba0
aliseyedabady <ialiseyed@gmail.com>
2024-07-07 11:12:41 +0330

    Store product and upload images in create product

--- src/controllers/product/index.ts
+++ src/controllers/product/index.ts
@@ -2,19 +2,30 @@ import { Request, Response } from "express";
 import upload from "../../config/multer";
 import { ResponseHandler } from "../../lib";
 import { validationResult } from "express-validator";
+import { Product, Image } from "../../models";
 
 class ProductController {
   async create(req: Request, res: Response) {
-    upload(req, res, async (err) => {
-      if (err) {
-        return ResponseHandler.error(res, err);
-      }
-    });
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return ResponseHandler.validationError(res, errors);
     }
     try {
+      const product = new Product(req.body);
+      await product.save();
+      if (req.files && Array.isArray(req.files)) {
+        const imageDocs = req?.files?.map(
+          (file) =>
+            new Image({
+              url: `/uploads/${file.filename}`,
+              productId: product._id,
+            })
+        );
+        await Image.insertMany(imageDocs);
+
+        return ResponseHandler.success(res, { ...product, imageDocs });
+      }
+      return ResponseHandler.success(res, product);
     } catch (error) {
       return ResponseHandler.error(res, error);
     }
