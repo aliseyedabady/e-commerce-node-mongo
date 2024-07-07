99c5f21d8a7538fd56d7ffbf3d3ec05c8d05c110
aliseyedabady <ialiseyed@gmail.com>
2024-07-07 13:23:04 +0330

    Add checkUniqueUpdate to utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 import config from "../config/env";
 import { IUser } from "../interfaces";
-import { TCheckExist, TCheckUnique } from "./type";
+import { TCheckExist, TCheckUnique, TCheckUniqueUpdate } from "./type";
 
 export const generateAccessToken = (user: IUser) => {
   return jwt.sign({ id: user._id, isAdmin: user.isAdmin }, config.JWT_SECRET, {
@@ -39,7 +39,6 @@ export const checkExist = async ({
   }
 };
 
-
 export const checkUnique = async ({
   value,
   model,
@@ -52,4 +51,24 @@ export const checkUnique = async ({
       return Promise.reject(message);
     }
   }
+};
+
+export const checkUniqueUpdate = async ({
+  model,
+  key,
+  value,
+  id,
+  message,
+}: TCheckUniqueUpdate) => {
+  const existingProduct = await model.findOne({ [key]: value });
+
+  if (!existingProduct) {
+    return true;
+  }
+
+  if (id && existingProduct._id.toString() === id) {
+    return true;
+  }
+
+  return Promise.reject(message);
 };
