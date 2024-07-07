500dd96ec4261a6368676e06aaddf5f295bcf8ed
aliseyedabady <ialiseyed@gmail.com>
2024-07-07 20:08:15 +0330

    Sort images of product

--- src/controllers/product/index.ts
+++ src/controllers/product/index.ts
@@ -2,17 +2,18 @@ import { Request, Response } from "express";
 import { validationResult } from "express-validator";
 import { ResponseHandler } from "../../lib";
 import { Image, Product } from "../../models";
+import { RequestUser } from "../../middlewares/type";
 
 class ProductController {
-  async create(req: Request, res: Response) {
+  async create(req: RequestUser, res: Response) {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return ResponseHandler.validationError(res, errors);
     }
     try {
-      const product = new Product(req.body);
+      const product = new Product({ ...req.body, ownerId: req.user?._id });
       await product.save();
-      if (req.files && Array.isArray(req.files)) {
+      if (req.files && Array.isArray(req.files) && req.files.length > 0) {
         const imageDocs = req?.files?.map(
           (file) =>
             new Image({
@@ -22,8 +23,12 @@ class ProductController {
         );
         await Image.insertMany(imageDocs);
 
-        return ResponseHandler.success(res, { ...product, imageDocs });
+        return ResponseHandler.success(res, {
+          ...product,
+          images: imageDocs.map((ele) => ele.url),
+        });
       }
+      console.log({ product });
       return ResponseHandler.success(res, product);
     } catch (error) {
       return ResponseHandler.error(res, error);
