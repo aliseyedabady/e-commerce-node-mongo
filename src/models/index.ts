835d5e664d4f69c3cd998f78aecfc52e960eafc3
aliseyedabady <ialiseyed@gmail.com>
2024-07-07 11:12:49 +0330

    Add Image model to index.ts

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,5 +1,6 @@
 import Category from "./Category";
+import Image from "./Image";
 import Product from "./Product";
 import User from "./User";
 
-export { User, Category, Product };
+export { User, Category, Product, Image };
