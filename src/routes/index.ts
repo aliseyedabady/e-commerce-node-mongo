b32675354341bc9c6c24239384cee1197c532460
aliseyedabady <ialiseyed@gmail.com>
2024-07-07 11:11:54 +0330

    Add multi images for upload

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,7 @@ import { AuthController, ProductController } from "../controllers";
 import { adminLoginValidation, signupValidation } from "../validations";
 import config from "../config/env";
 import { createProductValidation } from "../validations/createProduct";
+import upload from "../config/multer";
 
 const api = Router();
 
@@ -13,7 +14,12 @@ if (config.MODE === "development") {
 api.post("/admin/login", adminLoginValidation, AuthController.adminLogin);
 api.use("/admin", AdminMiddleware);
 
-api.post("/admin/products", createProductValidation, ProductController.create);
+api.post(
+  "/admin/products",
+  upload.array("images", 10),
+  createProductValidation,
+  ProductController.create
+);
 api.get("/admin/products", ProductController.findAll);
 api.get("/admin/products/:id", ProductController.findOne);
 api.put("/admin/products/:id", ProductController.update);
