399cb6a90cdd3103c17787eaf6f8c413546e5b8d
aliseyedabady <ialiseyed@gmail.com>
2024-07-07 13:22:28 +0330

    Format this file

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,17 @@
 import ResponseHandler from "./ResponseHandler";
-import { generateOTP, generateAccessToken, checkExist , checkUnique } from "./utils";
+import {
+  generateOTP,
+  generateAccessToken,
+  checkExist,
+  checkUnique,
+  checkUniqueUpdate,
+} from "./utils";
 
-export { generateOTP, ResponseHandler, generateAccessToken, checkExist , checkUnique };
+export {
+  generateOTP,
+  ResponseHandler,
+  generateAccessToken,
+  checkExist,
+  checkUnique,
+  checkUniqueUpdate,
+};
