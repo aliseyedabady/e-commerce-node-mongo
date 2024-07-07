935edb9d454a4f24996848d339e4be87fb2a2296
aliseyedabady <ialiseyed@gmail.com>
2024-07-07 10:27:42 +0330

    Add product routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { AdminMiddleware } from "../middlewares";
-import { AuthController } from "../controllers";
+import { AuthController, ProductController } from "../controllers";
 import { adminLoginValidation, signupValidation } from "../validations";
 import config from "../config/env";
+import { createProductValidation } from "../validations/createProduct";
 
 const api = Router();
 
@@ -10,5 +11,12 @@ if (config.MODE === "development") {
   api.post("/signup", signupValidation, AuthController.signup);
 }
 api.post("/admin/login", adminLoginValidation, AuthController.adminLogin);
+api.use("/admin", AdminMiddleware);
+
+api.post("/admin/products", createProductValidation, ProductController.create);
+api.get("/admin/products", ProductController.findAll);
+api.get("/admin/products/:id", ProductController.findOne);
+api.put("/admin/products/:id", ProductController.update);
+api.delete("/admin/products/:id", ProductController.delete);
 
 export default api;
