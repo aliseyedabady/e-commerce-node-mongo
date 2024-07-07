81ccdbaedd6860e6c5f2aba0faf5f7e8b98094da
aliseyedabady <ialiseyed@gmail.com>
2024-07-07 19:03:55 +0330

    Add softDeleteById method to statics

--- src/plugins/softDelete.ts
+++ src/plugins/softDelete.ts
@@ -5,8 +5,11 @@ export const softDeletePlugin = (schema: Schema): void => {
   const excludeDeletedItems = function (this: Query<any, any>) {
     this.where({ isDeleted: { $ne: true } });
   };
-  schema.pre<Query<any, any>>('find', excludeDeletedItems);
-  schema.pre<Query<any, any>>('findOne', excludeDeletedItems);
-  schema.pre<Query<any, any>>('findOneAndUpdate', excludeDeletedItems);
-  schema.pre<Query<any, any>>('countDocuments', excludeDeletedItems);
+  schema.pre<Query<any, any>>("find", excludeDeletedItems);
+  schema.pre<Query<any, any>>("findOne", excludeDeletedItems);
+  schema.pre<Query<any, any>>("findOneAndUpdate", excludeDeletedItems);
+  schema.pre<Query<any, any>>("countDocuments", excludeDeletedItems);
+  schema.statics.softDeleteById = async function (id: string) {
+    return this.findByIdAndUpdate(id, { isDeleted: true }, { new: true });
+  };
 };
